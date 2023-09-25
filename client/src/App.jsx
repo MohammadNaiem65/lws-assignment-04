2e722e00e2f951b429a4b6f61fd33356ae87711a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-09-25 08:50:16 +0600

    completed the initital react setup

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import AddBookForm from './components/AddBookForm/AddBookForm';
+import BookContainer from './components/BookContainer/BookContainer';
+import ListHeader from './components/ListHeader/ListHeader';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+	return (
+		<div className='bg-gray-300'>
+			{/* //  Navbar  */}
+			<Navbar />
+
+			<main className='py-12 2xl:px-6'>
+				<div className='container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8'>
+					<div className='order-2 xl:-order-1'>
+						{/* Book list header */}
+						<ListHeader />
+
+						{/* Book container */}
+						<BookContainer />
+					</div>
+
+					{/* Add book form */}
+					<AddBookForm />
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default App;
